c00ab31637421118f48c3a5c61508c079f092fa0
Pierre Cruz <contact@pierrecruz.com>
2020-04-14 16:08:53 +0200

    Start #223

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-navigation";
 import { Text } from "react-native-elements";
+import { requestPermissionsAsync } from "expo-location";
 
 // Components
 import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync();
+    } catch (e) {
+      setErrorMessage(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h2>Create a Track</Text>
       <Map />
+      {errorMessage ? <Text>Please enable location services</Text> : null}
     </SafeAreaView>
   );
 };
